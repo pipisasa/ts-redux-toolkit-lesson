f0be65f2599791bf871bbb20ff551d6dbcee71a8
pipisasa <emir0725mk@gmail.com>
2021-11-15 16:57:51 +0600

    Axios interceptors example

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,6 +2,24 @@ import Axios from 'axios';
 import { API_URL } from './constants';
 
 export const $api = Axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL, 
 });
 
+
+// $api.interceptors.request.use(
+//   config => {
+//     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+//     return config;
+//   }
+// );
+
+// $api.interceptors.response.use(
+//   response => response,
+//   error => {
+//     if (error.response.status === 401) {
+//       localStorage.removeItem('token');
+//       window.location.href = '/login';
+//     }
+//     return Promise.reject(error);
+//   }
+// );
